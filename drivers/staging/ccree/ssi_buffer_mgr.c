0b6a434cf15be60a5f7ab202e7294756008d717f
Logan Gunthorpe <logang@deltatee.com>
2017-02-08 12:44:52 -0700

    scatterlist: Modify SG copy functions to support io memory.
    Now that we are using p2pmem SG buffers we occasionally have to copy
    to and from this memory. For this, we add an iomem flag to
    sg_copy_buffer for copying with iomemcpy. We also add the sg_iocopy_
    variants to use this more easily.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Stephen Bates <sbates@raithlin.com>
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>

--- drivers/staging/ccree/ssi_buffer_mgr.c
+++ drivers/staging/ccree/ssi_buffer_mgr.c
@@ -142,7 +142,7 @@ void ssi_buffer_mgr_copy_scatterlist_portion(
 
 	nents = ssi_buffer_mgr_get_sgl_nents(sg, end, &lbytes, NULL);
 	sg_copy_buffer(sg, nents, (void *)dest, (end - to_skip + 1), to_skip,
-		       (direct == SSI_SG_TO_BUF));
+		       (direct == SSI_SG_TO_BUF), false);
 }
 
 static inline int ssi_buffer_mgr_render_buff_to_mlli(
