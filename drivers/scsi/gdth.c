73796a2f2d39bd1c941d2c1319a977c426833b7d
Logan Gunthorpe <logang@deltatee.com>
2017-04-05 14:29:32 -0600

    scsi: hisi_sas, mvsas, gdth: Make use of the new sg_kmap helper function
    Very straightforward conversion of three scsi drivers.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- drivers/scsi/gdth.c
+++ drivers/scsi/gdth.c
@@ -2301,10 +2301,15 @@ static void gdth_copy_internal_data(gdth_ha_str *ha, Scsi_Cmnd *scp,
                 return;
             }
             local_irq_save(flags);
-            address = kmap_atomic(sg_page(sl)) + sl->offset;
+            address = sg_kmap(sl, SG_KMAP_ATOMIC);
+            if (IS_ERR(address)) {
+                scp->result = DID_ERROR << 16;
+                return;
+	    }
+
             memcpy(address, buffer, cpnow);
             flush_dcache_page(sg_page(sl));
-            kunmap_atomic(address);
+            sg_kunmap(sl, address, SG_KMAP_ATOMIC);
             local_irq_restore(flags);
             if (cpsum == cpcount)
                 break;
