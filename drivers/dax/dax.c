cdc274e3c36919841caf373102b533f6d87ce5a2
Pushkar Jambhlekar <pushkar.iit@gmail.com>
2017-04-17 01:23:17 +1000

    drivers/dax/dax.c: avoid potential deadlock
    dax_dev_huge_fault() returns without releasing rcu_read_lock().  Make code
    change to avoid this situation
    
    Link: http://lkml.kernel.org/r/1491888405-7772-1-git-send-email-pushkar.iit@gmail.com
    Signed-off-by: Pushkar Jambhlekar <pushkar.iit@gmail.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Johannes Thumshirn <jthumshirn@suse.de>
    Cc: Ross Zwisler <ross.zwisler@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- drivers/dax/dax.c
+++ drivers/dax/dax.c
@@ -590,7 +590,7 @@ static int dax_dev_huge_fault(struct vm_fault *vmf,
 		rc = __dax_dev_pud_fault(dax_dev, vmf);
 		break;
 	default:
-		return VM_FAULT_FALLBACK;
+		rc = VM_FAULT_FALLBACK;
 	}
 	rcu_read_unlock();
 
