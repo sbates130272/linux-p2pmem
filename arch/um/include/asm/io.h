e4e2c85897aaa49d1c86210774a18559736492c6
Logan Gunthorpe <logang@deltatee.com>
2017-05-12 12:12:20 -0600

    um: add dummy ioremap and iounmap functions
    The user mode architecture does not provide ioremap or iounmap, and
    because of this, the arch won't build when the functions are used in some
    core libraries.
    
    I have designs to use these functions in scatterlist.c where they'd
    almost certainly never be called on the um architecture but it does need
    to compile. Thus, if the function is ever hit it returns NULL.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Stephen Bates <sbates@raithlin.com>
    Cc: Jeff Dike <jdike@addtoit.com>
    Cc: Richard Weinberger <richard@nod.at>
    Cc: Al Viro <viro@ZenIV.linux.org.uk>

---
+++ arch/um/include/asm/io.h
@@ -0,0 +1,17 @@
+#ifndef _ASM_UM_IO_H
+#define _ASM_UM_IO_H
+
+#define ioremap ioremap
+static inline void __iomem *ioremap(phys_addr_t offset, size_t size)
+{
+	return NULL;
+}
+
+#define iounmap iounmap
+static inline void iounmap(void __iomem *addr)
+{
+}
+
+#include <asm-generic/io.h>
+
+#endif
