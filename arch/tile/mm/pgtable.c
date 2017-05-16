0d0fa6148d5a64f7340532b72b9baa6c326d8e5e
Logan Gunthorpe <logang@deltatee.com>
2017-05-16 15:31:23 -0600

    tile: provide default ioremap declaration
    Add a default ioremap function which was not provided in all
    circumstances. (Only when CONFIG_PCI and CONFIG_TILEGX was set).
    
    I have designs to use them in scatterlist.c where they'd likely never
    be called with this architecture, but it is needed to compile.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    Signed-off-by: Stephen Bates <sbates@raithlin.com>

--- arch/tile/mm/pgtable.c
+++ arch/tile/mm/pgtable.c
@@ -503,6 +503,19 @@ void __iomem *ioremap_prot(resource_size_t phys_addr, unsigned long size,
 }
 EXPORT_SYMBOL(ioremap_prot);
 
+#if !defined(CONFIG_PCI) || !defined(CONFIG_TILEGX)
+/* ioremap is conditionally declared in pci_gx.c */
+
+void __iomem *ioremap(resource_size_t phys_addr, unsigned long size)
+{
+	pgprot_t pgprot = {};
+
+	return ioremap_prot(phys_addr, size, pgprot);
+}
+EXPORT_SYMBOL(ioremap);
+
+#endif
+
 /* Unmap an MMIO VA mapping. */
 void iounmap(volatile void __iomem *addr_in)
 {
