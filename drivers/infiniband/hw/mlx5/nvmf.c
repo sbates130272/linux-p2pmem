a3534a3712903bab412bce2406c4585f98c2fa0c
Max Gurtovoy <maxg@mellanox.com>
2017-05-17 23:13:06 +0000

    IB/mlx5: implement new NVMEoF offload PRM commands
    New commands:
     - mlx5_ib_create_nvmf_backend_ctrl
     - mlx5_ib_destroy_nvmf_backend_ctrl
     - mlx5_ib_attach_nvmf_ns
     - mlx5_ib_detach_nvmf_ns
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- drivers/infiniband/hw/mlx5/nvmf.c
+++ drivers/infiniband/hw/mlx5/nvmf.c
@@ -30,6 +30,11 @@
  * SOFTWARE.
  */
 
+#include <linux/module.h>
+#include <linux/mlx5/qp.h>
+#include <linux/mlx5/srq.h>
+#include <linux/mlx5/nvmf.h>
+
 #include "mlx5_ib.h"
 
 void mlx5_ib_internal_fill_nvmf_caps(struct mlx5_ib_dev *dev)
@@ -75,4 +80,131 @@ void mlx5_ib_internal_fill_nvmf_caps(struct mlx5_ib_dev *dev)
 	caps->max_data_offset = MLX5_CAP_NVMF(mdev, max_icdoff);
 
 	return;
+}
+
+static void set_nvmf_backend_ctrl_attrs(struct ib_nvmf_backend_ctrl_init_attr *attr,
+					struct mlx5_be_ctrl_attr *in)
+{
+	in->cq_page_offset = attr->cq_page_offset;
+	in->sq_page_offset = attr->sq_page_offset;
+	in->cq_log_page_size = attr->cq_log_page_size;
+	in->sq_log_page_size = attr->sq_log_page_size;
+	in->initial_cqh_db_value = attr->initial_cqh_db_value;
+	in->initial_sqt_db_value = attr->initial_sqt_db_value;
+	in->cqh_dbr_addr = attr->cqh_dbr_addr;
+	in->sqt_dbr_addr = attr->sqt_dbr_addr;
+	in->cq_pas = attr->cq_pas;
+	in->sq_pas = attr->sq_pas;
+}
+
+struct ib_nvmf_ctrl *mlx5_ib_create_nvmf_backend_ctrl(struct ib_srq *srq,
+			struct ib_nvmf_backend_ctrl_init_attr *init_attr)
+{
+	struct mlx5_ib_dev *dev = to_mdev(srq->device);
+	struct mlx5_ib_srq *msrq = to_msrq(srq);
+	struct mlx5_ib_nvmf_be_ctrl *ctrl;
+	struct mlx5_be_ctrl_attr in = {0};
+	int err;
+
+	ctrl = kzalloc(sizeof(*ctrl), GFP_KERNEL);
+	if (!ctrl)
+		return ERR_PTR(-ENOMEM);
+
+	set_nvmf_backend_ctrl_attrs(init_attr, &in);
+	err = mlx5_core_create_nvmf_backend_ctrl(dev->mdev,
+						 &msrq->msrq,
+						 &ctrl->mctrl,
+						 &in);
+	if (err) {
+		mlx5_ib_dbg(dev, "create NVMF backend ctrl failed, err %d\n", err);
+		goto err_ctrl;
+	}
+
+	mlx5_ib_dbg(dev, "create NVMF backend ctrl with ctrlid 0x%x\n",
+		    ctrl->mctrl.id);
+
+	ctrl->ibctrl.id = ctrl->mctrl.id;
+	return &ctrl->ibctrl;
+
+err_ctrl:
+	kfree(ctrl);
+
+	return ERR_PTR(err);
+
+}
+
+int mlx5_ib_destroy_nvmf_backend_ctrl(struct ib_nvmf_ctrl *ctrl)
+{
+	struct mlx5_ib_dev *dev = to_mdev(ctrl->srq->device);
+	struct mlx5_ib_nvmf_be_ctrl *mctrl = to_mctrl(ctrl);
+	struct mlx5_ib_srq *msrq = to_msrq(ctrl->srq);
+
+	mlx5_core_destroy_nvmf_backend_ctrl(dev->mdev,
+					    &msrq->msrq,
+					    &mctrl->mctrl);
+
+	kfree(mctrl);
+	return 0;
+}
+
+static void set_nvmf_ns_attrs(struct ib_nvmf_ns_init_attr *attr,
+			      struct mlx5_ns_attr *in)
+{
+	in->frontend_namespace = attr->frontend_namespace;
+	in->backend_namespace = attr->backend_namespace;
+	in->lba_data_size = attr->lba_data_size;
+	in->backend_ctrl_id = attr->backend_ctrl_id;
+}
+
+
+struct ib_nvmf_ns *mlx5_ib_attach_nvmf_ns(struct ib_nvmf_ctrl *ctrl,
+			struct ib_nvmf_ns_init_attr *init_attr)
+{
+	struct mlx5_ib_nvmf_be_ctrl *mctrl = to_mctrl(ctrl);
+	struct mlx5_ib_dev *dev = to_mdev(ctrl->srq->device);
+	struct mlx5_ib_srq *msrq = to_msrq(ctrl->srq);
+	struct mlx5_ib_nvmf_ns *ns;
+	struct mlx5_ns_attr in = {0};
+	int err;
+
+	ns = kzalloc(sizeof(*ns), GFP_KERNEL);
+	if (!ns)
+		return ERR_PTR(-ENOMEM);
+
+	set_nvmf_ns_attrs(init_attr, &in);
+	err = mlx5_core_attach_nvmf_ns(dev->mdev,
+				       &msrq->msrq,
+				       &mctrl->mctrl,
+				       &ns->mns,
+				       &in);
+	if (err) {
+		mlx5_ib_dbg(dev, "attach NVMF ns failed, err %d\n", err);
+		goto err_ns;
+	}
+
+	mlx5_ib_dbg(dev, "NVMF ns %p was attached\n", ns);
+
+	return &ns->ibns;
+
+err_ns:
+	kfree(ns);
+
+	return ERR_PTR(err);
+
+}
+
+int mlx5_ib_detach_nvmf_ns(struct ib_nvmf_ns *ns)
+{
+	struct mlx5_ib_nvmf_ns *mns = to_mns(ns);
+	struct mlx5_ib_nvmf_be_ctrl *mctrl = to_mctrl(ns->ctrl);
+	struct mlx5_ib_dev *dev = to_mdev(ns->ctrl->srq->device);
+	struct mlx5_ib_srq *msrq = to_msrq(ns->ctrl->srq);
+
+	mlx5_core_detach_nvmf_ns(dev->mdev,
+				 &msrq->msrq,
+				 &mctrl->mctrl,
+				 &mns->mns);
+
+	kfree(mns);
+	return 0;
 }
