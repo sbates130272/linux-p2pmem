3b6a852211c20d15bb19419333742fac980905e5
Max Gurtovoy <maxg@mellanox.com>
2017-05-17 17:39:37 +0000

    IB/core: Introduce NVMEoF offload capabilities
    IB devices should set IB_DEVICE_NVMF_TARGET_OFFLOAD cap and fill-in
    the ib_nvmf_caps if they support NVMEoF target side command
    capsules (CC) and response capsules (RC) offloading.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- drivers/infiniband/core/verbs.c
+++ drivers/infiniband/core/verbs.c
@@ -1062,7 +1062,8 @@ static const struct {
 						 IB_QP_ALT_PATH			|
 						 IB_QP_ACCESS_FLAGS		|
 						 IB_QP_MIN_RNR_TIMER		|
-						 IB_QP_PATH_MIG_STATE),
+						 IB_QP_PATH_MIG_STATE		|
+						 IB_QP_OFFLOAD_TYPE),
 				 [IB_QPT_XRC_INI] = (IB_QP_CUR_STATE		|
 						 IB_QP_ALT_PATH			|
 						 IB_QP_ACCESS_FLAGS		|
@@ -1096,7 +1097,8 @@ static const struct {
 						IB_QP_ACCESS_FLAGS		|
 						IB_QP_ALT_PATH			|
 						IB_QP_PATH_MIG_STATE		|
-						IB_QP_MIN_RNR_TIMER),
+						IB_QP_MIN_RNR_TIMER		|
+						IB_QP_OFFLOAD_TYPE),
 				[IB_QPT_XRC_INI] = (IB_QP_CUR_STATE		|
 						IB_QP_ACCESS_FLAGS		|
 						IB_QP_ALT_PATH			|
