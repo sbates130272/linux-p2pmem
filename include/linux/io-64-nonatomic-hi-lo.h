10e87212b9376cb5781c8ec2054739e2cb271e2a
Logan Gunthorpe <logang@deltatee.com>
2017-06-23 11:28:31 -0600

    io-64-nonatomic: add io{read|write}64[be]{_lo_hi|_hi_lo} macros
    This patch adds generic io{read|write}64[be]{_lo_hi|_hi_lo} macros if
    they are not already defined by the architecture. (As they are provided
    by the generic iomap library).
    
    The patch also points io{read|write}64[be] to the variant specified by the
    header name.
    
    This is because new drivers are encouraged to use ioreadXX, et al instead
    of readX[1], et al -- and mixing ioreadXX with readq is pretty ugly.
    
    [1] ldd3: section 9.4.2
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>
    cc: Christoph Hellwig <hch@lst.de>
    cc: Arnd Bergmann <arnd@arndb.de>
    cc: Alan Cox <gnomes@lxorguk.ukuu.org.uk>
    cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    squash! io-64-nonatomic: add io{read|write}64[be] macros

--- include/linux/io-64-nonatomic-hi-lo.h
+++ include/linux/io-64-nonatomic-hi-lo.h
@@ -54,4 +54,66 @@ static inline void hi_lo_writeq_relaxed(__u64 val, volatile void __iomem *addr)
 #define writeq_relaxed hi_lo_writeq_relaxed
 #endif
 
+#ifndef ioread64_hi_lo
+#define ioread64_hi_lo ioread64_hi_lo
+static inline u64 ioread64_hi_lo(void __iomem *addr)
+{
+	const u32 __iomem *p = addr;
+	u32 low, high;
+
+	high = ioread32(p + 1);
+	low = ioread32(p);
+
+	return low + ((u64)high << 32);
+}
+#endif
+
+#ifndef iowrite64_hi_lo
+#define iowrite64_hi_lo iowrite64_hi_lo
+static inline void iowrite64_hi_lo(u64 val, void __iomem *addr)
+{
+	iowrite32(val >> 32, addr + sizeof(u32));
+	iowrite32(val, addr);
+}
+#endif
+
+#ifndef ioread64be_hi_lo
+#define ioread64be_hi_lo ioread64be_hi_lo
+static inline u64 ioread64be_hi_lo(void __iomem *addr)
+{
+	const u32 __iomem *p = addr;
+	u32 low, high;
+
+	high = ioread32be(p);
+	low = ioread32be(p + 1);
+
+	return low + ((u64)high << 32);
+}
+#endif
+
+#ifndef iowrite64be_hi_lo
+#define iowrite64be_hi_lo iowrite64be_hi_lo
+static inline void iowrite64be_hi_lo(u64 val, void __iomem *addr)
+{
+	iowrite32be(val >> 32, addr);
+	iowrite32be(val, addr + sizeof(u32));
+}
+#endif
+
+#ifndef ioread64
+#define ioread64 ioread64_hi_lo
+#endif
+
+#ifndef iowrite64
+#define iowrite64 iowrite64_hi_lo
+#endif
+
+#ifndef ioread64be
+#define ioread64be(p) ioread64be_hi_lo
+#endif
+
+#ifndef iowrite64be
+#define iowrite64be(v, p) iowrite64be_hi_lo
+#endif
+
 #endif	/* _LINUX_IO_64_NONATOMIC_HI_LO_H_ */
