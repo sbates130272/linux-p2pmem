ef2298a724194b2c550fff2bfbe2ceec56e6bae5
Logan Gunthorpe <logang@deltatee.com>
2017-12-20 13:56:46 +0100

    pci-p2p: Add PCI p2pmem dma mappings to adjust the bus offset
    The DMA address used when mapping PCI P2P memory must be the PCI bus
    address. Thus, introduce pci_p2pmem_[un]map_sg() to map the correct
    addresses when using P2P memory.
    
    For this, we assume that an SGL passed to these functions contain all
    p2p memory or no p2p memory.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- include/linux/pci-p2p.h
+++ include/linux/pci-p2p.h
@@ -34,6 +34,8 @@ int pci_p2pmem_alloc_sgl(struct pci_dev *pdev, struct scatterlist **sgl,
 void pci_p2pmem_free_sgl(struct pci_dev *pdev, struct scatterlist *sgl,
 		unsigned int nents);
 void pci_p2pmem_publish(struct pci_dev *pdev, bool publish);
+int pci_p2pmem_map_sg(struct scatterlist *sg, int nents);
+void pci_p2pmem_unmap_sg(struct scatterlist *sg, int nents);
 #else /* CONFIG_PCI_P2P */
 static inline int pci_p2pmem_add_resource(struct pci_dev *pdev, int bar,
 		size_t size, u64 offset)
@@ -79,6 +81,13 @@ static inline void pci_p2pmem_free_sgl(struct pci_dev *pdev,
 {
 }
 static inline void pci_p2pmem_publish(struct pci_dev *pdev, bool publish)
+{
+}
+static inline int pci_p2pmem_map_sg(struct scatterlist *sg, int nents)
+{
+	return -ENODEV;
+}
+static inline void pci_p2pmem_unmap_sg(struct scatterlist *sg, int nents)
 {
 }
 #endif /* CONFIG_PCI_P2P */
