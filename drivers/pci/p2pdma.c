ace20b3af83bf70f89e331e1b79c04a426c0c17b
Logan Gunthorpe <logang@deltatee.com>
2018-03-01 15:21:38 -0700

    fixup! PCI/P2PDMA: Support peer-to-peer memory

--- drivers/pci/p2pdma.c
+++ drivers/pci/p2pdma.c
@@ -1,18 +1,10 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+// SPDX-License-Identifier: GPL-2.0
 /*
  * PCI Peer 2 Peer DMA support.
  *
  * Copyright (c) 2016-2017, Microsemi Corporation
  * Copyright (c) 2017, Christoph Hellwig.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
  */
 
 #include <linux/pci-p2pdma.h>
