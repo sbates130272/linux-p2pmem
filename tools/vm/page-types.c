17c543254211c433ac298e747546f760b09fcfe4
Andrew Morton <akpm@linux-foundation.org>
2018-06-28 16:30:00 +1000

    tools-adding-support-for-idle-page-tracking-to-tool-fix
    simplify mark_page_idle()
    
    Cc: Christian Hansen <chansen3@cisco.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- tools/vm/page-types.c
+++ tools/vm/page-types.c
@@ -597,21 +597,15 @@ static int mark_page_idle(unsigned long offset)
 	static uint64_t buf;
 	int len;
 
-	if ((offset / 64 == off / 64) || buf == 0) {
-		buf |= 1UL << (offset % 64);
-		off = offset;
-		return 0;
+	if ((offset / 64 != off / 64) && buf != 0) {
+		len = pwrite(page_idle_fd, &buf, 8, 8 * (off / 64));
+		if (len < 0) {
+			perror("mark page idle");
+			return len;
+		}
 	}
-
-	len = pwrite(page_idle_fd, &buf, 8, 8 * (off / 64));
-	if (len < 0) {
-		perror("mark page idle");
-		return len;
-	}
-
 	buf = 1UL << (offset % 64);
 	off = offset;
-
 	return 0;
 }
 
