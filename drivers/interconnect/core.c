randomplum/kernel-msm

interconnect: Add debugfs support
Add a functionality to provide information about the current constraints
per each node and provider.

Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

drivers/interconnect/core.c
@@ -6,6 +6,7 @@
  * Author: Georgi Djakov <georgi.djakov@linaro.org>
  */
 
+#include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/init.h>
 #include <linux/interconnect-consumer.h>
@@ -14,9 +15,11 @@
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
 
 static DEFINE_MUTEX(interconnect_provider_list_mutex);
 static LIST_HEAD(interconnect_provider_list);
+static struct dentry *interconnect_debugfs_dir;
 
 /**
  * struct interconnect_path - interconnect path structure
@@ -382,3 +385,71 @@ EXPORT_SYMBOL_GPL(interconnect_del_provider);
 MODULE_AUTHOR("Georgi Djakov <georgi.djakov@linaro.org");
 MODULE_DESCRIPTION("Interconnect Driver Core");
 MODULE_LICENSE("GPL v2");
+
+#ifdef CONFIG_DEBUG_FS
+
+static void interconnect_summary_show_one(struct seq_file *s,
+					  struct interconnect_node *n)
+{
+	if (!n)
+		return;
+
+	seq_printf(s, "%p %12d %12d\n",
+		   n, n->creq.avg_bw, n->creq.peak_bw);
+}
+
+static int interconnect_summary_show(struct seq_file *s, void *data)
+{
+	struct icp *icp;
+
+	seq_puts(s, "            node        avg         peak\n");
+	seq_puts(s, "------------------------------------------\n");
+
+	mutex_lock(&interconnect_provider_list_mutex);
+
+	list_for_each_entry(icp, &interconnect_provider_list, icp_list) {
+		struct interconnect_node *n;
+
+		list_for_each_entry(n, &icp->nodes, icn_list) {
+			interconnect_summary_show_one(s, n);
+		}
+	}
+
+	mutex_unlock(&interconnect_provider_list_mutex);
+
+	return 0;
+}
+
+
+static int interconnect_summary_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, interconnect_summary_show, inode->i_private);
+}
+
+static const struct file_operations interconnect_summary_fops = {
+	.open		= interconnect_summary_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+
+static int __init interconnect_debugfs_init(void)
+{
+	struct dentry *file;
+
+	interconnect_debugfs_dir = debugfs_create_dir("interconnect", NULL);
+	if (!interconnect_debugfs_dir) {
+		pr_err("interconnect: error creating debugfs directory\n");
+		return -ENODEV;
+	}
+
+	file = debugfs_create_file("interconnect_summary", 0444,
+				   interconnect_debugfs_dir, NULL,
+				   &interconnect_summary_fops);
+	if (!file)
+		return -ENODEV;
+
+	return 0;
+}
+late_initcall(interconnect_debugfs_init);
+#endif

sbates130272/linux-p2pmem

drivers/interconnect/core.c
@@ -6,6 +6,7 @@
  * Author: Georgi Djakov <georgi.djakov@linaro.org>
  */
 
+#include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/idr.h>
 #include <linux/init.h>
@@ -17,10 +18,12 @@
 #include <linux/slab.h>
 #include <linux/of.h>
 #include <linux/overflow.h>
+#include <linux/uaccess.h>
 
 static DEFINE_IDR(icc_idr);
 static LIST_HEAD(icc_provider_list);
 static DEFINE_MUTEX(icc_lock);
+static struct dentry *icc_debugfs_dir;
 
 /**
  * struct icc_req - constraints that are attached to each node
@@ -48,6 +51,62 @@ struct icc_path {
 	size_t num_nodes;
 	struct icc_req reqs[];
 };
+
+#ifdef CONFIG_DEBUG_FS
+
+static void icc_summary_show_one(struct seq_file *s, struct icc_node *n)
+{
+	if (!n)
+		return;
+
+	seq_printf(s, "%-30s %12d %12d\n",
+		   n->name, n->avg_bw, n->peak_bw);
+}
+
+static int icc_summary_show(struct seq_file *s, void *data)
+{
+	struct icc_provider *provider;
+
+	seq_puts(s, " node                                   avg         peak\n");
+	seq_puts(s, "--------------------------------------------------------\n");
+
+	mutex_lock(&icc_lock);
+
+	list_for_each_entry(provider, &icc_provider_list, provider_list) {
+		struct icc_node *n;
+
+		list_for_each_entry(n, &provider->nodes, node_list) {
+			struct icc_req *r;
+
+			icc_summary_show_one(s, n);
+			hlist_for_each_entry(r, &n->req_list, req_node) {
+				if (!r->dev)
+					continue;
+
+				seq_printf(s, "    %-26s %12d %12d\n",
+					   dev_name(r->dev), r->avg_bw,
+					   r->peak_bw);
+			}
+		}
+	}
+
+	mutex_unlock(&icc_lock);
+
+	return 0;
+}
+
+static int icc_summary_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, icc_summary_show, inode->i_private);
+}
+
+static const struct file_operations icc_summary_fops = {
+	.open		= icc_summary_open,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+#endif
 
 static struct icc_node *node_find(const int id)
 {
@@ -645,6 +704,25 @@ int icc_provider_del(struct icc_provider *provider)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(icc_provider_del);
+
+static int __init icc_init(void)
+{
+#ifdef CONFIG_DEBUG_FS
+	icc_debugfs_dir = debugfs_create_dir("interconnect", NULL);
+	debugfs_create_file("interconnect_summary", 0444,
+			    icc_debugfs_dir, NULL, &icc_summary_fops);
+#endif
+	return 0;
+}
+
+static void __exit icc_exit(void)
+{
+#ifdef CONFIG_DEBUG_FS
+	debugfs_remove_recursive(icc_debugfs_dir);
+#endif
+}
+module_init(icc_init);
+module_exit(icc_exit);
 
 MODULE_AUTHOR("Georgi Djakov <georgi.djakov@linaro.org");
 MODULE_DESCRIPTION("Interconnect Driver Core");