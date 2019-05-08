55837be333b3e59159468c924ada27cccafe9ccc
Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
2019-05-07 21:45:03 -0700

    nvmet-passthru: add passthru code to process commands
    Add passthru command handling capability for the NVMeOF target and
    export passthru APIs which are used to integrate passthru
    code with nvmet-core. A passthru ns member is added to the target request
    to hold the ns reference for respective commands.
    
    The new file io-cmd-passthru.c handles passthru cmd parsing and execution.
    In the passthru mode, we create a block layer request from the nvmet
    request and map the data on to the block layer request. For handling the
    side effects of the passthru admin commands we add two functions similar
    to the nvme_passthru[start|end]() functions present in the nvme-core.
    Only admin commands on a white list are let through which includes
    vendor unique commands.
    
    We introduce new passthru workqueue similar to the one we have for the
    file backend for NVMeOF target to execute the NVMe Admin passthru
    commands.
    
    All the new passthru code is enabled or disabled by a new KConfig option
    for the NVMeOF target.
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    [logang@deltatee.com:
       * renamed passthru-cmd.c to io-cmd-passthru.c for consistency
       * squashed "update target makefile for passthru"
       * squashed "integrate passthru request processing"
       * squashed "update KConfig with config passthru option"
       * added appropriate CONFIG_NVME_TARGET_PASSTHRU #ifdefs
       * pushed passthru_wq into passthrtu.c and introduced
         nvmet_passthru_init() and nvmet_passthru_destroy() to avoid
         inline #ifdef mess
       * renamed nvmet_passthru_ctrl() to nvmet_req_passthru_ctrl()
         and provided nvmet_passthr_ctrl() to get the ctrl from a subsys
       * fixed failure path in nvmet_passthru_execute_cmd() to ensure
         we always complete the request (with an error when appropriate)
       * restructered out nvmet_passthru_make_request() and
         nvmet_passthru_execute_cmd() to create nvmet_passthru_map_sg()
         which makes the code simpler and more readable.
       * move call to nvme_find_get_ns() into nvmet_passthru_execute_cmd()
         to prevent a lockdep error. nvme_find_get_ns() takes a
         lock and can sleep but nvme_init_req() is called while
         hctx_lock() is held (in the loop transport) and therefore
         should not sleep.
       * added check in nvmet_passthru_execute_cmd() to ensure we don't
         violate queue_max_segments or queue_max_hw_sectors.
       * added nvmet_passthru_set_mdts() to prevent requests that
         exceed max_segments
       * convert admin command blacklist to a white list with vendor
         unique commands specifically allowed
       * force setting cmic bit to support multipath for connections
         to the target
       * dropped le16_to_cpu() conversion in nvmet_passthru_req_done() as
         it's currently already done in nvme_end_request()
       * unabbreviated 'VUC' in a comment as it's not a commonly known
         acronym
       * removed unnecessary inline tags on static functions
       * minor edits to commit message
    ]
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- drivers/nvme/target/core.c
+++ drivers/nvme/target/core.c
@@ -896,6 +896,8 @@ bool nvmet_req_init(struct nvmet_req *req, struct nvmet_cq *cq,
 	if (unlikely(!req->sq->ctrl))
 		/* will return an error for any Non-connect command: */
 		status = nvmet_parse_connect_cmd(req);
+	else if (nvmet_req_passthru_ctrl(req))
+		status = nvmet_parse_passthru_cmd(req);
 	else if (likely(req->sq->qid != 0))
 		status = nvmet_parse_io_cmd(req);
 	else if (nvme_is_fabrics(req->cmd))
@@ -1463,11 +1465,15 @@ static int __init nvmet_init(void)
 
 	nvmet_ana_group_enabled[NVMET_DEFAULT_ANA_GRPID] = 1;
 
+	error = nvmet_passthru_init();
+	if (error)
+		goto out;
+
 	buffered_io_wq = alloc_workqueue("nvmet-buffered-io-wq",
 			WQ_MEM_RECLAIM, 0);
 	if (!buffered_io_wq) {
 		error = -ENOMEM;
-		goto out;
+		goto out_passthru_destroy;
 	}
 
 	error = nvmet_init_discovery();
@@ -1483,6 +1489,8 @@ out_exit_discovery:
 	nvmet_exit_discovery();
 out_free_work_queue:
 	destroy_workqueue(buffered_io_wq);
+out_passthru_destroy:
+	nvmet_passthru_destroy();
 out:
 	return error;
 }
@@ -1493,6 +1501,7 @@ static void __exit nvmet_exit(void)
 	nvmet_exit_discovery();
 	ida_destroy(&cntlid_ida);
 	destroy_workqueue(buffered_io_wq);
+	nvmet_passthru_destroy();
 
 	BUILD_BUG_ON(sizeof(struct nvmf_disc_rsp_page_entry) != 1024);
 	BUILD_BUG_ON(sizeof(struct nvmf_disc_rsp_page_hdr) != 1024);
