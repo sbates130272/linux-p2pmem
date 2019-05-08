5ef2e58d39b671eeaa4a21c9c94f5da8ce27230a
Logan Gunthorpe <logang@deltatee.com>
2019-05-07 21:45:03 -0700

    nvmet-passthru: Add passthru code to process commands
    Add passthru command handling capability for the NVMeOF target and
    export passthru APIs which are used to integrate passthru
    code with nvmet-core.
    
    The new file passthru.c handles passthru cmd parsing and execution.
    In the passthru mode, we create a block layer request from the nvmet
    request and map the data on to the block layer request. Only admin
    commands on a white list are let through which includes vendor specific
    commands.
    
    Based-on-a-patch-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- include/linux/nvme.h
+++ include/linux/nvme.h
@@ -858,6 +858,7 @@ enum nvme_admin_opcode {
 	nvme_admin_security_recv	= 0x82,
 	nvme_admin_sanitize_nvm		= 0x84,
 	nvme_admin_get_lba_status	= 0x86,
+	nvme_admin_vendor_start		= 0xC0,
 };
 
 #define nvme_admin_opcode_name(opcode)	{ opcode, #opcode }
