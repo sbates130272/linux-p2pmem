4f2506e271971558cd25299cbcba6b113b8f8c1d
Logan Gunthorpe <logang@deltatee.com>
2020-04-23 16:54:26 -0600

    PCI/P2PDMA: Introduce pci_p2pdma_should_map_bus() and pci_p2pdma_bus_offset()
    Introduce pci_p2pdma_should_map_bus() which is meant to be called by
    DMA map functions to determine how to map a given p2pdma page.
    
    pci_p2pdma_bus_offset() is also added to allow callers to get the bus
    offset if they need to map the bus address.
    
    Signed-off-by: Logan Gunthorpe <logang@deltatee.com>

--- include/linux/pci-p2pdma.h
+++ include/linux/pci-p2pdma.h
@@ -34,6 +34,8 @@ int pci_p2pdma_map_sg_attrs(struct device *dev, struct scatterlist *sg,
 		int nents, enum dma_data_direction dir, unsigned long attrs);
 void pci_p2pdma_unmap_sg_attrs(struct device *dev, struct scatterlist *sg,
 		int nents, enum dma_data_direction dir, unsigned long attrs);
+u64 pci_p2pdma_bus_offset(struct page *page);
+int pci_p2pdma_dma_map_type(struct device *dev, struct dev_pagemap *pgmap);
 int pci_p2pdma_enable_store(const char *page, struct pci_dev **p2p_dev,
 			    bool *use_p2pdma);
 ssize_t pci_p2pdma_enable_show(char *page, struct pci_dev *p2p_dev,
@@ -82,6 +84,15 @@ static inline void pci_p2pmem_free_sgl(struct pci_dev *pdev,
 }
 static inline void pci_p2pmem_publish(struct pci_dev *pdev, bool publish)
 {
+}
+static inline u64 pci_p2pdma_bus_offset(struct page *page)
+{
+	return -1;
+}
+static inline int pci_p2pdma_dma_map_type(struct device *dev,
+					  struct dev_pagemap *pgmap)
+{
+	return -1;
 }
 static inline int pci_p2pdma_map_sg_attrs(struct device *dev,
 		struct scatterlist *sg, int nents, enum dma_data_direction dir,
