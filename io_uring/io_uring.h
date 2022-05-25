91963c665f4031098f343c43813a16f106c34811
Jens Axboe <axboe@kernel.dk>
2022-05-24 21:19:47 -0600

    io_uring: split out splice related operations
    This splits out splice and tee support.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- io_uring/io_uring.h
+++ io_uring/io_uring.h
@@ -9,10 +9,25 @@ enum {
 	IOU_ISSUE_SKIP_COMPLETE	= -EIOCBQUEUED,
 };
 
+static inline void req_set_fail(struct io_kiocb *req)
+{
+	req->flags |= REQ_F_FAIL;
+	if (req->flags & REQ_F_CQE_SKIP) {
+		req->flags &= ~REQ_F_CQE_SKIP;
+		req->flags |= REQ_F_SKIP_LINK_CQES;
+	}
+}
+
 static inline void io_req_set_res(struct io_kiocb *req, s32 res, u32 cflags)
 {
 	req->cqe.res = res;
 	req->cqe.flags = cflags;
+}
+
+static inline void io_put_file(struct file *file)
+{
+	if (file)
+		fput(file);
 }
 
 void __io_req_complete32(struct io_kiocb *req, unsigned int issue_flags,
@@ -20,5 +35,9 @@ void __io_req_complete32(struct io_kiocb *req, unsigned int issue_flags,
 void __user *io_buffer_select(struct io_kiocb *req, size_t *len,
 			      unsigned int issue_flags);
 unsigned int io_put_kbuf(struct io_kiocb *req, unsigned issue_flags);
+
+struct file *io_file_get_normal(struct io_kiocb *req, int fd);
+struct file *io_file_get_fixed(struct io_kiocb *req, int fd,
+			       unsigned issue_flags);
 
 #endif
